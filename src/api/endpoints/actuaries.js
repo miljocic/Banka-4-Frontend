16ebf137cfce2829f37293a1b0276072392ff772
DunjaRakocevic <drakocevic14423rn@raf.rs>
2026-04-08 21:27:18 +0200

    fix za actuaries

--- src/api/endpoints/actuaries.js
+++ src/api/endpoints/actuaries.js
@@ -1,7 +1,7 @@
 import api from '../client';
 
 export const actuariesApi = {
-  getAll:         (params)        => api.get('/actuaries', { params }),
-  changeLimit:    (id, newLimit)  => api.patch(`/actuaries/${id}`, { limit: newLimit }),
+    getAll:         (params = {})    => api.get('/actuaries', { params: { page: 1, page_size: 20, ...params } }),
+    changeLimit:    (id, newLimit)  => api.patch(`/actuaries/${id}`, { limit: newLimit }),
   resetUsedLimit: (id)            => api.post(`/actuaries/${id}/reset-used-limit`, {}),
 };
